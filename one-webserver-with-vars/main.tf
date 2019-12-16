af89ef8ce9a0a6ae94a1e7b0332608e37396b70a
mouliballa <moulibala@outlook.com>
2019-12-16 19:22:10 +0530

    Update main.tf

--- one-webserver-with-vars/main.tf
+++ one-webserver-with-vars/main.tf
@@ -42,10 +42,4 @@ resource "google_compute_firewall" "http-server" {
   target_tags   = ["http-server"]
 }
 
-output "ip" {
-  value = "${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}"
-}
 
-output "frewal-rule" {
-value = "${google_compute_firewall.http-server.name}"
-}
